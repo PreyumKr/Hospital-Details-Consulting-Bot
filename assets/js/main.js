f8fcd1ccc67cede117e1d12d3d52357a8ed97b2c
preyumkr <preyumkrsingh@gmail.com>
2021-12-27 00:55:00 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Medilab - v4.7.0
-* Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
